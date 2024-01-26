1bab31ade953ab7eb556ff65837f476b0cf568e3
Guilherme Martins <guimsl2004@gmail.com>
2024-01-26 10:31:34 -0300

    sessao 16 position

---
+++ JogoXadrez/Program/Program.java
@@ -0,0 +1,11 @@
+package JogoXadrez.Program;
+
+import JogoXadrez.BoardGame.Position;
+
+public class Program {
+    public static void main(String[]args){
+        Position position = new Position(3, 5);
+        System.out.println(position);
+    }
+    
+}
