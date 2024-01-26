39a070402b001fa4928e6fc6cea65564949970e0
Guilherme Martins <guimsl2004@gmail.com>
2024-01-26 10:39:13 -0300

    sessao 16 board

--- JogoXadrez/BoardGame/Board.java
+++ JogoXadrez/BoardGame/Board.java
@@ -1,30 +1,31 @@
 package JogoXadrez.BoardGame;
 
-public class Position {
+public class Board {
     private int row;
     private int column;
+    private Piece[][] pieces;
 
-    public Position(int row, int column) {
+    public Board(int row, int column {
         this.row = row;
         this.column = column;
+        pieces = new Piece[row][column];
     }
-    
-    @Override
-    public String toString(){
-        return row + ", "+ column;
-    }
+
     public int getRow() {
         return row;
     }
+
     public void setRow(int row) {
         this.row = row;
     }
+
     public int getColumn() {
         return column;
     }
+
     public void setColumn(int column) {
         this.column = column;
     }
-
+    
     
 }
