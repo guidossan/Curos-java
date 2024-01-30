2e1a4e730091feccc61d18c0e1379f6fa23cba44
Guilherme Martins <guimsl2004@gmail.com>
2024-01-30 10:14:05 -0300

    Exception

--- JogoXadrez/BoardGame/Board.java
+++ JogoXadrez/BoardGame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int row, int column) {
+        if (row < 1 || column < 1 ){
+            throw new BoardException("Error, board must be more than 1 row and column");
+        }
         this.row = row;
         this.column = column;
         pieces = new Piece[row][column];
@@ -15,27 +18,39 @@ public class Board {
     }
 
     public Piece piece (Position position){
+        if (!positionExists(position)){
+            throw new BoardException("Position not in the board");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
     public void placePiece(Piece piece, Position position){
+        if(thereThisaPiece(position)){
+            throw new BoardException("There is a peace in this position");
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+    private boolean positionExists(int rows, int columns){
+        return rows >=0 && rows < row && columns >=0 && columns< column;
+    }
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+    public boolean thereThisaPiece(Position position){
+        if (!positionExists(position)){
+            throw new BoardException("Position not in the board");
+        }
+        return piece(position)!=null;
     }
 
     public int getRow() {
         return row;
     }
 
-    public void setRow(int row) {
-        this.row = row;
-    }
+
 
     public int getColumn() {
         return column;
-    }
-
-    public void setColumn(int column) {
-        this.column = column;
     }
     
     
---
+++ JogoXadrez/BoardGame/BoardException.java
@@ -0,0 +1,8 @@
+package JogoXadrez.BoardGame;
+
+public class BoardException extends RuntimeException {
+    private static final long serialVersionUID = 1l;
+    public BoardException(String msg){
+        super(msg);
+    }
+}
