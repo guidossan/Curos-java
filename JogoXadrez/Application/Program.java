133aa712852d66724273026c703338f483f90fe3
Guilherme Martins <guimsl2004@gmail.com>
2024-01-30 10:41:06 -0300

    sla

--- JogoXadrez/Application/Program.java
+++ JogoXadrez/Application/Program.java
@@ -1,8 +1,9 @@
 package JogoXadrez.Application;
+import JogoXadrez.Chess.ChessException;
 import JogoXadrez.Chess.ChessMatch;
 
 public class Program {
-    public static void main(String[]args){
+    public static void main(String[]args) throws ChessException{
         ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
        
--- aula01/src/Aula1_OO.java
+++ aula01/src/Aula1_OO.java
@@ -1,9 +1,8 @@
-import java.util.Locale;
-import java.util.Scanner;
+
 
 public class Aula1_OO {
     public static void main(String [] args){
-        L
+    
 
 
 
